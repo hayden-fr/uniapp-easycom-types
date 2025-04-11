f77ee141a3dfd19237e36d61cddb3d6ace8b505a
hayden <48267247+hayden-fr@users.noreply.github.com>
2025-04-11 17:13:53 +0800

    pref: merge utils to index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,41 @@
+import { readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import type { Plugin } from 'vite'
 import { globSync } from 'fast-glob'
-import { pascalCase, requireJson } from './utils'
 import { writeFileSync } from 'node:fs'
+
+const requireJson = <T = any>(
+  filename: string,
+  defaultValue: T,
+): T | undefined => {
+  try {
+    const content = readFileSync(filename, 'utf-8')
+    const contentWithoutComments = content.replace(
+      /\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g,
+      (m, g) => (g ? '' : m),
+    )
+    return JSON.parse(contentWithoutComments)
+  } catch (error) {
+    return defaultValue
+  }
+}
+
+const pascalCase = (str: string) => {
+  if (!str || typeof str !== 'string') return ''
+
+  const words = str
+    .replace(/[-_]/g, ' ')
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    .replace(/\s+/g, ' ')
+    .trim()
+    .split(' ')
+
+  const pascalCase = words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join('')
+
+  return pascalCase
+}
 
 export interface EasycomOptions {
   dts?: string
